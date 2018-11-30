cd54b36d18d4f2ef5c3e60f4ac04f87b7177e339
stonewallstan <stanleyarmstrong31@gmail.com>
2018-11-29 18:06:26 -0800

    Updates

---
+++ precache-manifest.590739a3d436400f2945874ab41f3f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "feffc9803ae1668cd61a",
+    "url": "/birthday-website/static/js/runtime~main.feffc980.js"
+  },
+  {
+    "revision": "771a39bef8e867958d10",
+    "url": "/birthday-website/static/js/main.771a39be.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/birthday-website/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "771a39bef8e867958d10",
+    "url": "/birthday-website/static/css/main.edd86787.chunk.css"
+  },
+  {
+    "revision": "d0b0b53b2995b6a6774b3cd0026dc4b1",
+    "url": "/birthday-website/index.html"
+  }
+];
